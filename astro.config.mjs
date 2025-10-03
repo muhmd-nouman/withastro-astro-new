fd9456bbcd4cbfe8bee7c5e5b3289a8c1214ea53
Muhammad <rebeccafashion771@gmail.com>
2025-10-03 12:45:13 +0500

    Configure for Vercel deployment with SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,20 +3,21 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
 	site: 'https://invisiblesymbol.com',
 	integrations: [mdx(), sitemap(), tailwind(), react()],
-	adapter: node({
-		mode: 'standalone'
-	}),
 	output: 'server',
-	server: {
-		port: 4321,
-		host: true
-	},
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+		speedInsights: {
+			enabled: true,
+		},
+	}),
 	vite: {
 		define: {
 			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
