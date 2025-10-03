1db3397dfcc6d51f10a89f207009641846ad76cb
Muhammad <rebeccafashion771@gmail.com>
2025-10-03 13:17:03 +0500

    Update Vercel adapter configuration

--- src/pages/api/entry.ts
+++
@@ -1,8 +0,0 @@
-import type { APIRoute } from 'astro';
-import { createServer } from '@astrojs/node';
-
-const server = await createServer();
-
-export const all: APIRoute = async (context) => {
-  return await server.fetch(context.request);
-}
